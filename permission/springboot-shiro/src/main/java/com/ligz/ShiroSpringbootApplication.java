bf4afcd0899a428d63267cffd0da255740301783
esmusssein777 <380449395@qq.com>
2019-04-29 15:03:13 +0800

    关于shiro的权限的springboot框架

---
+++ permission/springboot-shiro/src/main/java/com/ligz/ShiroSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.ligz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author:ligz
+ */
+@SpringBootApplication
+public class ShiroSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroSpringbootApplication.class);
+    }
+}
